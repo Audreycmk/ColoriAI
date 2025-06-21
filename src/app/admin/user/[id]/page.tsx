961196ffcec73739b77d1c5cbc6e681cac3fc681
Audrey <Audrey@macbookair.home>
2025-06-21 01:01:41 -0400

    Remove unused Link import from admin user page

--- src/app/admin/user/[id]/page.tsx
+++ src/app/admin/user/[id]/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
-import Link from 'next/link';
 
 interface ColorExtraction {
   label: string;
